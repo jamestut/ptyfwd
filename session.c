18d0b3788805527f73e6523f70238a7a3af60ccf
James <j1@ionbox.in>
2022-01-15 19:09:06 +1100

    Session manager uses unified map code.

--- session.c
+++ session.c
@@ -1,118 +1,73 @@
 #include "session.h"
+#include "simplemap.h"
 #include <stdlib.h>
 #include <err.h>
 #include <unistd.h>
 #include <string.h>
 
-#define HASHMAP_SIZE 128
-#define MAX_SESSIONS 1024
-
-typedef struct session_node session_node_t;
-
-struct session_node {
-  session_node_t *prev;
-  session_node_t *next;
-  struct session data;
-};
-
-struct {
-  session_node_t *map[HASHMAP_SIZE];
-  size_t count;
-} sessdata = {};
-
-session_node_t *add_to_hashmap(uint64_t sessid);
-
-session_node_t *get_session_node(uint64_t sessid);
+static void *map_sessid = NULL;
 
 uint64_t new_session(struct session **newsess) {
-  uint64_t sessid = INVALID_SESSION_ID;
-
-  if (sessdata.count >= MAX_SESSIONS) {
-    warnx("Maximum session count exceeded.");
+  if (!map_sessid) {
+    map_sessid = simplemap_init();
+  }
+  if (!map_sessid) {
+    warnx("Error allocating map.");
+    return INVALID_SESSION_ID;
+  }
+  if (simplemap_count(map_sessid) >= SIMPLEMAP_MAX_ENTRIES) {
+    warnx("Maximum session count exceeded. Rejecting request.");
     return INVALID_SESSION_ID;
   }
 
+  uint64_t sessid = INVALID_SESSION_ID;
+
   for (;;) {
     arc4random_buf(&sessid, sizeof(sessid));
-    if (sessid != INVALID_SESSION_ID && !get_session_node(sessid)) {
-      session_node_t *newnode = add_to_hashmap(sessid);
-      if (!newnode) {
-        warn("Error allocating session data");
+    void *dummy;
+    if (sessid != INVALID_SESSION_ID && !simplemap_get(map_sessid, sessid, &dummy)) {
+      struct session *sess = malloc(sizeof(struct session));
+      if (!sess) {
+        warnx("Error allocating session data.");
         return INVALID_SESSION_ID;
       }
-      *newsess = &newnode->data;
-      ++sessdata.count;
+      sess->id = sessid;
+      sess->childpid = sess->fdpair[0] = sess->fdpair[1] = -1;
+
+      if (!simplemap_add(map_sessid, sessid, sess)) {
+        free(sess);
+        warnx("Error adding session data to map.");
+        return INVALID_SESSION_ID;
+      }
+
+      *newsess = sess;
       return sessid;
     }
   }
 }
 
-session_node_t *add_to_hashmap(uint64_t sessid) {
-  session_node_t *ret = calloc(sizeof(session_node_t), 1);
-  if (!ret) {
-    return NULL;
-  }
-  ret->data.id = sessid;
-  memset(ret->data.fdpair, -1, sizeof(ret->data.fdpair));
-
-  session_node_t *node = sessdata.map[sessid % HASHMAP_SIZE];
-  ret->next = node;
-  if (node) {
-    node->prev = ret;
-  }
-  sessdata.map[sessid % HASHMAP_SIZE] = ret;
-
-  return ret;
-}
-
 void delete_session(uint64_t sessid) {
-  session_node_t *node = get_session_node(sessid);
-  if (!node) {
+  struct session *sess = get_session(sessid);
+  if (!sess) {
     return;
   }
 
   // close unix socket
   for (int i = 0; i < 2; ++i) {
-    if (node->data.fdpair[i] >= 0) {
-      close(node->data.fdpair[i]);
-      node->data.fdpair[i] = -1;
+    if (sess->fdpair[i] >= 0) {
+      close(sess->fdpair[i]);
+      sess->fdpair[i] = -1;
     }
   }
 
-  // remove from hashmap
-  if (node->prev) {
-    node->prev->next = node->next;
-  } else {
-    // this is a head node
-    sessdata.map[sessid % HASHMAP_SIZE] = node->next;
-  }
-  if (node->next) {
-    node->next->prev = node->prev;
-  }
-  --sessdata.count;
-
-  free(node);
-}
-
-session_node_t *get_session_node(uint64_t sessid) {
-  session_node_t *node = sessdata.map[sessid % HASHMAP_SIZE];
-  if (!node) {
-    return NULL;
-  }
-
-  while (node) {
-    if (node->data.id == sessid) {
-      break;
-    }
-    node = node->next;
-  }
-  return node;
+  simplemap_del(map_sessid, sessid);
+  free(sess);
 }
 
 struct session *get_session(uint64_t sessid) {
-  session_node_t *ret = get_session_node(sessid);
-  if (!ret) {
+  struct session *sess;
+  if (!simplemap_get(map_sessid, sessid, (void **)&sess)) {
     return NULL;
   }
-  return &ret->data;
+  return sess;
 }
--- session.h
+++ session.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <stdint.h>
+#include <unistd.h>
 
 #define INVALID_SESSION_ID 0ULL
 
 struct session {
   uint64_t id;
+  pid_t childpid;
   int fdpair[2];
 };
 
